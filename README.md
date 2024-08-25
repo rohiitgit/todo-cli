e01d34f73bf835fffdce79a13d28048aa272a623
rohiitgit <rohitcodes03@gmail.com>
2024-08-25 22:06:41 +0530

    finished adding all functionalities

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # todo-cli
+
+## Requirements 
+commander
+
+Set up locally with:
+```
+git clone https://github.com/rohiitgit/todo-cli.git
+cd todo-cli
+npm install commander
+```
+.
+
+## How to Use
+Usage: node todos.js [command] <str>
+
+CLI to add, remove and mark a todo as done.
+
+Options:
+  -V, --version   output the version number
+  -h, --help      display help for command
+
+Commands:
+  add <string>    adds a todo
+  del <string>    deletes a todo
+  mark <string>   marks todo as done
+  help [command]  display help for command
--- todo.js
+++ todo.js
@@ -2,46 +2,69 @@ const { Command } = require('commander');
 const program = new Command();
 const fs = require('fs');
 
+
 program
-  .name('todo-cli')
-  .description('CLI to add, remove, edit todos and mark a todo as done.')
-  .version('0.0.1');
+    .name('todo-cli')
+    .description('CLI to add, remove, edit todos and mark a todo as done.')
+    .version('0.0.1');
 
 program.command('add')
-  .description('adds a todo')
-  .argument('<string>', 'todo to add')
-  .action((str) => {
-    const jsonString = JSON.stringify(str)
-    fs.appendFile('todos.json', jsonString, (err) => {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log('todo was added')
-        }
-    })
-  });
+    .description('adds a todo')
+    .argument('<string>', 'todo to add')
+    .action((str) => {
+        const jsonString = JSON.stringify(str)
+        fs.appendFile('todos.json', jsonString, (err) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log('todo was added')
+            }
+        })
+    });
 
 
-  program.command('del')
-  .description('deletes a todo')
-  .argument('<string>', 'todo to add')
-  .action((str) => {
-    fs.readFile('todos.json', "utf-8", (err, data) => {
-        if (err) {
-            console.log(err)
-            return
-        } else {
-            const updatedData = data.replace(new RegExp(str, 'g'), '').trim();
-            fs.writeFile('todos.json', updatedData, 'utf-8', (err) => {
-                if (err) {
-                    console.log(err)
-                    return;
-                } else {
-                    console.log('Deleted todo')
-                }
-            });
-        }
-    })
-  });
+program.command('del')
+    .description('deletes a todo')
+    .argument('<string>', 'todo to add')
+    .action((str) => {
+        fs.readFile('todos.json', "utf-8", (err, data) => {
+            if (err) {
+                console.log(err)
+                return
+            } else {
+                const updatedData = data.replace(new RegExp(str, 'g'), '').trim();
+                fs.writeFile('todos.json', updatedData, 'utf-8', (err) => {
+                    if (err) {
+                        console.log(err)
+                        return;
+                    } else {
+                        console.log('Deleted todo')
+                    }
+                });
+            }
+        })
+    });
+
+program.command('mark')
+    .description('marks todo as done')
+    .argument('<string>', 'marks todo as done')
+    .action((str) => {
+        fs.readFile('todos.json', "utf-8", (err, data) => {
+            if (err) {
+                console.log(err)
+                return
+            } else {
+                const updatedData = data.replace(new RegExp(str, 'g'), str+':done').trim();
+                fs.writeFile('todos.json', updatedData, 'utf-8', (err) => {
+                    if (err) {
+                        console.log(err)
+                        return;
+                    } else {
+                        console.log('Marked todo as done')
+                    }
+                });
+            }
+        })
+    });
 
 program.parse();
